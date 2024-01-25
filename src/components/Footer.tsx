676d82663ce5008256e3d76de656cbe3591ec2a2
Vilmar Fonseca <vilmarfonsec@gmail.com>
2024-01-25 13:18:23 -0300

    fix: footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import UnderlineLink from '@/components/links/UnderlineLink';
 
 export default function Footer() {
   return (
-    <footer className='relative sm:absolute bottom-2 text-gray-700 w-full flex justify-center'>
+    <footer className='relative sm:absolute bottom-2 text-gray-700 w-full flex gap-1 justify-center'>
       © {new Date().getFullYear()} By{' '}
       <UnderlineLink href='https://vilmarfonseca.com'>
         Vilmar Fonseca
