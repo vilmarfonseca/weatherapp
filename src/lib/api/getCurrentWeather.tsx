cc248c7420f71ba9fdd1f44faa684fcd45f4e778
Vilmar Fonseca <vilmarfonsec@gmail.com>
2024-01-25 03:56:09 -0300

    chore: added env for app URL

--- src/lib/api/getCurrentWeather.tsx
+++ src/lib/api/getCurrentWeather.tsx
@@ -8,7 +8,7 @@ export const getCurrentWeather = async ({
   units: string;
 }) => {
   const data = await fetch(
-    `http://localhost:3000/api/weather/?lat=${lat}&lon=${lon}&units=${units}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/weather/?lat=${lat}&lon=${lon}&units=${units}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
   );
   if (!data.ok) {
     throw new Error('Failed to fetch data');
